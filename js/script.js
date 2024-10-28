b6a58af96164b0ac153ca87a172ded9dbf34b2e7
Guilherme Machado Neves <gui.m.neves01@gmail.com>
2024-10-28 08:17:15 -0300

    atualizacao sobreMIm

--- js/script.js
+++ js/script.js
@@ -25,3 +25,30 @@ initTypingAnimation()
 
 
 
+
+
+document.querySelectorAll('.icon').forEach(icon => {
+    icon.addEventListener('mouseover', () => {
+        // Esconde todas as imagens
+        document.querySelectorAll('.containerDemoProjetos > div').forEach(container => {
+            container.style.display = 'none';
+        });
+
+        // Identifica o contêiner a ser mostrado
+        const containerToShow = document.querySelector(`.containerFotos${icon.classList[0].replace('icon', '')}`);
+        
+        // Verifica se o contêiner a ser mostrado é válido
+        if (containerToShow) {
+            containerToShow.style.display = 'flex'; // Mostra a imagem correspondente
+        } else {
+            console.log('Container não encontrado para:', icon.classList[0]);
+        }
+    });
+
+    icon.addEventListener('mouseout', () => {
+        // Esconde todas as imagens novamente
+        document.querySelectorAll('.containerDemoProjetos > div').forEach(container => {
+            container.style.display = 'none';
+        });
+    });
+});
