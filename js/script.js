976ff634e9bfe0304e1d34399c8ac184106dcebe
Guilherme Machado Neves <gui.m.neves01@gmail.com>
2024-11-26 11:41:50 -0400

    .

--- js/script.js
+++ js/script.js
@@ -52,3 +52,21 @@ document.querySelectorAll('.icon').forEach(icon => {
         });
     });
 });
+
+
+var radio = document.querySelector(".manualBtn");
+var cont = 1;
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaImagem()
+},5000);
+
+function proximaImagem(){
+    cont++
+    if(cont>3){
+        cont = 1;
+    }
+    document.getElementById('radio' +cont).checked = true
+}
